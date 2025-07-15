5a84b5205d32203b0fbb3da4ea36c4e696c3440e
tifjaflixe <tifjaflixe@gmail.com>
2025-07-15 14:38:49 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Bokep Xpanas';
-export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Amoy Viral Colmek Sma Cindo Tante China';
-export const url = 'https://bokepxpanas.pages.dev';
-export const terbit = '2025-01-27T00:01:03Z';
